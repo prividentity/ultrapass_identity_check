d9dc402823ed2e8d2ed5bd2cc4763ccbbc566893
Haroon Jawad <haroonjawad6@gmail.com>
2023-02-21 18:15:03 +0500

    Removed unnecessary files

--- hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as useCamera } from "./useCamera";
-export { default as useWasm } from "./useWasm";
-export { default as usePredictAge } from "./usePredictAge";
-export { default as useCameraPermissions } from "./useCameraPermissions";
