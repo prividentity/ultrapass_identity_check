2756fc13c7867ae860fa83d61f7f8700d9a5a4e8
Clark Evangelista <cjamesevangelista@gmail.com>
2023-03-21 01:19:30 +0800

    intermittent error fixes, updated dl scan message

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,37 +1,44 @@
 export const getBackDocumentMessage = (id: number) => {
-    switch(id){
-        case 0:
-            return "Success"
-        case -12:
-        case -1:
-            return "Please show the back of DL in the camera screen"
-        case 3:
-            return "Reading barcode data, Please hold"
-        case -9:
-            return "Image is blurry. Please try to move DL closer or further."
-        case -2:
-            return "Api key issue"
-        default:
-            return ""
-    }
-}
-
+  switch (id) {
+    case 0:
+      return "Success";
+    case -12:
+    case -1:
+      return "MOVE CLOSER";
+    case 3:
+    case 10:
+      return "ALMOST DONE . . .";
+    case -9:
+      return "TOO BLURRY. PLEASE MOVE BACK";
+    case -2:
+      return "SYSTEM ERROR. Please try again later.";
+    default:
+      return "";
+  }
+};
 
 export const getScanFrontMessage = (status: number) => {
-    switch(status){
-        case 0:
-            return "Success"
-        case -12:
-        case -1:
-            return "Please show the front of your DL in the camera screen"
-        case 3:
-        case 999:
-            return "Please hold position."
-        case -9:
-            return "Image is blurry. Please try to move DL closer or further."
-        case -2:
-            return "Api key issue"
-        default:
-            return ""
-    }
-}
+  console.log("MESSAGE STATUS??? ", status);
+  switch (status) {
+    case 0:
+      return "Success";
+    case 12:
+    case -1:
+    case 18:
+      return "MOVE CLOSER";
+    case 3:
+    case 999:
+      return "MOVE JUST A LITTLE CLOSER";
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      return "";
+    case 9:
+      return "TOO BLURRY PLEASE MOVE BACK";
+    case -2:
+      return "SYSTEM ERROR. Please try again later.";
+    default:
+      return "";
+  }
+};
