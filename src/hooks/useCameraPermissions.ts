6c05d4b0a5d6b2ca9ffbed04bb5c77b6e8817d7b
Shiven Juneja <shiven.juneja@gmail.com>
2023-03-21 17:07:44 +0530

    fix for firefox browser

--- src/hooks/useCameraPermissions.ts
+++ src/hooks/useCameraPermissions.ts
@@ -1,30 +1,43 @@
 import { useEffect, useState } from "react";
+import { detect } from "detect-browser";
 
-const useCameraPermissions = (callback?: (arg0: boolean) => void) => {
+const browser = detect();
+
+const useCameraPermissions = (callback?: (arg0: boolean) => any) => {
   const [isCameraGranted, setIsCameraGranted] = useState(false);
   const checkCameraPermissions = async () => {
     navigator.permissions.query({ name: "camera" } as any).then((result) => {
       if (result.state === "granted") {
         setIsCameraGranted(true);
       } else {
-        navigator.mediaDevices
-          .getUserMedia({ video: true })
-          .then(function (stream) {
-            setIsCameraGranted(true);
-          })
-          .catch(function (err) {
-            setIsCameraGranted(false);
-            callback && callback(false);
-          });
       }
       // Don't do anything if the permission was denied.
     });
   };
   useEffect(() => {
-    checkCameraPermissions();
+    if (browser?.name === "firefox") {
+      getUserMedia(setIsCameraGranted, callback);
+    } else {
+      checkCameraPermissions();
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return { isCameraGranted };
 };
+
+const getUserMedia = (
+  setIsCameraGranted: (arg0: boolean) => void,
+  callback?: (arg0: boolean) => any
+) =>
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      setIsCameraGranted(true);
+    })
+    .catch(function (err) {
+      setIsCameraGranted(false);
+      callback && callback(false);
+    });
 
 export default useCameraPermissions;
