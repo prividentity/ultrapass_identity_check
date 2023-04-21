92af0f2e2c8c0a348b774231a9005641b7ef1c0a
Clark Evangelista <cjamesevangelista@gmail.com>
2023-04-21 21:25:58 +0800

    ts error fix

--- src/hooks/usePredictAge.ts
+++ src/hooks/usePredictAge.ts
@@ -5,37 +5,7 @@ const usePredictAge = () => {
   const [age, setAge] = useState(null);
   const [predictAgeHasFinished, setPredictAgeHasFinished] = useState(false);
 
-  const predictAgeCallback = (response?: {
-    returnValue: {
-      faces: any;
-    };
-    ageFactor?: number;
-    channels?: number;
-    conf_level_age?: number;
-    crop_conf_score?: number;
-    crop_conf_score_thr?: number;
-    crop_status?: number;
-    exposure?: number;
-    face_blurr_var?: number;
-    face_center_x?: number;
-    face_center_y?: number;
-    face_count?: number;
-    face_height?: number;
-    face_wear_eye_glass_score?: number;
-    face_wear_mask_score?: number;
-    face_wear_ok?: number;
-    face_width?: number;
-    im_height?: number;
-    im_width?: number;
-    image_iso_bytes?: number;
-    image_iso_height?: number;
-    image_iso_width?: number;
-    metric_bitfield?: number;
-    payload_type?: string;
-    qrate?: number;
-    result?: number;
-    result_age?: number;
-  }) => {
+  const predictAgeCallback = (response?: any) => {
     // console.log("RESPONSE USEPREDICT FE: ", response);
 
     const { faces } = response?.returnValue || {};
@@ -62,7 +32,7 @@ const usePredictAge = () => {
   };
 
   const doPredictAge = async () => {
-    const data = await predictAge(undefined, predictAgeCallback);
+    await predictAge(undefined, predictAgeCallback);
     // console.log(
     //   229,
     //   `${(performance as any).memory.usedJSHeapSize / Math.pow(1000, 2)} MB`
