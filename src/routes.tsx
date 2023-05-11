3de23b8fa92787092ec51c52ea4fe7e4b606a012
Clark Evangelista <cjamesevangelista@gmail.com>
2023-05-12 02:28:01 +0800

    update urls

--- src/routes.tsx
+++ src/routes.tsx
@@ -16,30 +16,18 @@ const router = createBrowserRouter(
   skin === "stncharms"
     ? [
         {
-            path: "/",
-            element: <Redirect Url="https://stncharms.com/" />,
-            errorElement: <Redirect Url="https://stncharms.com/" />,
-        },
-        {
-          path: "/cams_test",
-          element: <Home theme={themeName} skin={skin} />,
-        },
-        {
-          path: "/signup",
-          element: <Register theme={themeName} skin={skin} />,
-        },
-        {
-          path: "/signin",
-          element: <Signin theme={themeName} skin={skin} />,
-        },
-        {
           path: "/register",
           element: <Register theme={themeName} skin={skin} />,
+        },
+        {
+          path: "*",
+          element: <Redirect Url="https://stncharms.com/" />,
+          errorElement: <Redirect Url="https://stncharms.com/" />,
         },
       ]
     : [
         {
-          path: "/",
+          path: "/cams_test",
           element: <Home theme={themeName} skin={skin} />,
           errorElement: <Redirect Url="/" />,
         },
